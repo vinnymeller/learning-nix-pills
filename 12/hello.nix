b8816d531a66b64a8cd1701ef9e9d0a7745be4b1
Vinny Meller <vinnymeller@gmail.com>
2023-03-13 00:59:34 -0500

    nix and sh files

---
+++ 12/hello.nix
@@ -0,0 +1,6 @@
+{ mkDerivation }:
+
+mkDerivation {
+    name = "hello";
+    src = ./hello-2.10.tar.gz;
+}
---
+++ 9/autotools.nix
@@ -0,0 +1,11 @@
+pkgs: attrs:
+    with pkgs;
+    let defaultAttrs = {
+        builder = "${bash}/bin/bash";
+        args = [ ./builder.sh ];
+        baseInputs = [ gnutar gzip gnumake gcc coreutils gawk gnused gnugrep binutils.bintools findutils patchelf ];
+        buildInputs = [];
+        system = builtins.currentSystem;
+    };
+    in
+    derivation (defaultAttrs // attrs)
